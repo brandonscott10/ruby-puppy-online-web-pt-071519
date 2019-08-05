c42491567a901e13cd93002b9b2ab752fe059571
Brandon Parris <brandonparrisguy@gmail.com>
2019-08-05 14:21:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  def initialize 
+    @name = name 
+  end 
+  
+  def self.all
+    @all
+  end 
+  
+  def self.print_all
+    puts @@all 
+  end 
